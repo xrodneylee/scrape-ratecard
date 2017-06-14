9f5c876eef5eecabd62e31e1b7f9a9354c048a69
Lee Guan Pu <xrodneylee@infinitiessoft.com>
2017-06-14 17:35:51 +0800

    BeautifulSoup servey

--- scrape-google.py
+++ scrape-google.py
@@ -1,10 +1,18 @@
 from selenium import webdriver
+from bs4 import BeautifulSoup
 import sys
 import time
 
+url = 'https://aws.amazon.com/tw/ec2/pricing/on-demand/'
+
 driver = webdriver.Chrome('chromedriver.exe')
-driver.get('https://aws.amazon.com/tw/ec2/pricing/on-demand/')
+driver.get(url)
 pageSource = driver.page_source
-print(pageSource.encode(sys.stdin.encoding, "replace").decode(sys.stdin.encoding))
+# print(pageSource.encode(sys.stdin.encoding, "replace").decode(sys.stdin.encoding))
+soup = BeautifulSoup(pageSource, 'lxml')
+
+# print(soup.prettify(encoding='utf-8'))
+for caption in soup.find_all('caption'):
+  print('caption', caption.get_text())
 
 driver.close()
