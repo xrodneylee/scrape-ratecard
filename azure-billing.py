d173936a7291c6bd6128fa396474e25dc04698d5
xrodneylee <xrodneylee@gmail.com>
2017-06-16 00:32:18 +0800

    updated

--- azure-billing.py
+++ azure-billing.py
@@ -6,17 +6,22 @@ vm = set()
 with open('usage-daily.json') as usage_file:
   usage_data = json.load(usage_file)
 
-# with open('ratecard-TWD.json') as ratecard_file:
-#   ratecard_data = json.load(ratecard_file)
-
 for usage in usage_data['value']:
     meterCategory.add(usage['properties']['meterCategory'])
+    # instance_data = json.loads(usage['properties']['instanceData'])
+    # print(instance_data['Microsoft.Resources'])
     if usage['properties']['meterCategory'] == 'Virtual Machines':
         vm.add((usage['properties']['meterId'], usage['properties']['quantity']))
 
 
 print(meterCategory)
 print(vm)
+
+
+# with open('ratecard-TWD.json') as ratecard_file:
+#   ratecard_data = json.load(ratecard_file)
+
+
     #   if usage['properties'][vm]['meterCategory'] == 'Virtual Machines':
     #       print(usage['id'])
 
