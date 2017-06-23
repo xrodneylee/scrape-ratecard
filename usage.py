1b1d812f9c4aacc5a513460f6c293d56963399a8
Lee Guan Pu <xrodneylee@infinitiessoft.com>
2017-06-23 16:26:46 +0800

    refactor

---
+++ usage.py
@@ -0,0 +1,14 @@
+from database import db_session
+
+vm = set()
+meter_category = set()
+
+with open('usage-daily.json') as usage_file:
+  usage_data = json.load(usage_file)
+
+for usage in usage_data['value']:
+    meter_category.add(usage['properties']['meterCategory'])
+    # instance_data = json.loads(usage['properties']['instanceData'])
+    # print(instance_data['Microsoft.Resources'])
+    if usage['properties']['meterCategory'] == 'Virtual Machines':
+        vm.add((usage['properties']['meterId'], usage['properties']['quantity']))
