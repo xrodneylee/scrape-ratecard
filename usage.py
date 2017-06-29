f802a6776b6ce3b3edd7a2c97bb2ed39fbd144be
Lee Guan Pu <xrodneylee@infinitiessoft.com>
2017-06-29 17:16:02 +0800

    pylint

--- usage.py
+++ usage.py
@@ -8,30 +8,29 @@ meter_category = set()
 instance = []
 
 class Azure(object):
-    
+
     def __init__(self):
         pass
-    
-    def import_usage(self):
-      with open('usage-hourly.json') as usage_file:
-          usage_data = json.load(usage_file)
 
-      for usage in usage_data['value']:
-          # if usage['properties']['meterCategory'] == 'Virtual Machines':
-          instance_data = json.loads(usage['properties']['instanceData'])
-          resourceUri = str(instance_data['Microsoft.Resources']['resourceUri']).split(sep='/')
-          record = AzureUsage(resourceUri[4],
-                              resourceUri[6],
-                              resourceUri[8],
-                              usage['properties']['subscriptionId'],
-                              datetime.strptime(usage['properties']['usageStartTime'], '%Y-%m-%dT%H:%M:%S+00:00'),
-                              datetime.strptime(usage['properties']['usageEndTime'], '%Y-%m-%dT%H:%M:%S+00:00'),
-                              usage['properties']['meterName'],
-                              usage['properties']['meterCategory'],
-                              usage['properties']['meterId'],
-                              usage['properties']['quantity'])
-          db_session.add(record)
-      db_session.commit()
+    def import_usage(self):
+        with open('usage-hourly.json') as usage_file:
+            usage_data = json.load(usage_file)
+
+        for usage in usage_data['value']:
+            instance_data = json.loads(usage['properties']['instanceData'])
+            resourceUri = str(instance_data['Microsoft.Resources']['resourceUri']).split(sep='/')
+            record = AzureUsage(resourceUri[4],
+                                resourceUri[6],
+                                resourceUri[8],
+                                usage['properties']['subscriptionId'],
+                                datetime.strptime(usage['properties']['usageStartTime'], '%Y-%m-%dT%H:%M:%S+00:00'),
+                                datetime.strptime(usage['properties']['usageEndTime'], '%Y-%m-%dT%H:%M:%S+00:00'),
+                                usage['properties']['meterName'],
+                                usage['properties']['meterCategory'],
+                                usage['properties']['meterId'],
+                                usage['properties']['quantity'])
+            db_session.add(record)
+        db_session.commit()
               # vm.add((usage['properties']['meterId'], usage['properties']['quantity']))
               # instance.append(usage['properties']['instanceData'])
         # instance_data = json.loads(usage['properties']['instanceData'])
@@ -39,10 +38,10 @@ class Azure(object):
         #   if usage['properties'][vm]['meterCategory'] == 'Virtual Machines':
         #       print(usage['id'])
 def main():
-  AzureUsage.query.delete()
-  azure = Azure()
-  azure.import_usage()
-  print(meter_category)
+    AzureUsage.query.delete()
+    azure = Azure()
+    azure.import_usage()
+    print(meter_category)
   # print(instance)
   # for record in instance:
   #   # print(record)
@@ -50,4 +49,5 @@ def main():
   #   print('\n')
 
 if __name__ == '__main__':
-  main()
+    main()
+    
